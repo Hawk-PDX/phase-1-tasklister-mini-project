f44d96ce5ebe821033199c16b220f8700d0f0107
Garrett <hawkpdx@icloud.com>
2024-11-21 14:43:10 -0800

    functioning events - project complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const taskDescription = document.querySelector(
+      "#new-task-description"
+    ).value;
+
+    const taskList = document.querySelector("#tasks");
+
+    const newTask = document.createElement("li");
+    newTask.textContent = taskDescription;
+
+    taskList.appendChild(newTask);
+
+    form.reset();
+  });
 });
